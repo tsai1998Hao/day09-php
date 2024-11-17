0fbc02573892b89173fbabc9324da84e3ac87fc4
tsailearn87code <142395260+tsailearn87code@users.noreply.github.com>
2024-11-17 23:32:58 +0800

    解決編輯的CORS問題

--- day09_api03.php
+++ day09_api03.php
@@ -1,4 +1,9 @@
 <?php
+// 解決CORS問題
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+// 解決CORS問題
 $SERVER_NAME="localhost";
 $USER_NAME="root";
 $PASSWORD="";
